02fb8a1cea44b41528c07c4c52e7e8e8602b274c
Kritika Regmi <krtkregmi@gmail.com>
2025-07-25 16:24:02 -0700

    Add Contains Duplicate Solution

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>(); //creating a hashset 
+        
+        
+        for (int n:nums){
+            if (set.contains(n)){ //if any number is already present, terminate the program and return true
+                return true;
+            }
+            set.add(n);  //otherwise add it to the set
+        }
+
+        return false;   //return false if no duplicate is found
+    }
+}
